379faeb7f44a11bd32f9efdfc5f49b3d7514b3f0
Eliel504 <julia3192016@outlook.com>
2020-12-10 22:36:50 -0300

    Adicionando calculos do INSS, salário líquido e IRRF, além de adicionar funcionalidade de recarregamento

--- Challenge-001/js/script.js
+++ Challenge-001/js/script.js
@@ -1,6 +1,9 @@
     let sal_bruto = document.getElementById('gross-salary')
     let dep = document.getElementById('dependents')
     let res_dep = document.getElementById('res_dependents')
+    let res_inss = document.getElementById('res_inss')
+    let res_liquido = document.getElementById('res_base-calc')
+    let res_irrf = document.getElementById('base-tax')
     let container = document.getElementById('container')
     let deduct = document.getElementById('deductions')
     let res_deduct = document.getElementById('res_deduction')
@@ -18,8 +21,6 @@
     let box_span5 = document.getElementById('box-span-id5')
     let box_span6 = document.getElementById('box-span-id6')
 
-
-
 function clica_calcula(){
  
     if(sal_bruto.value <= 0){
@@ -30,21 +31,68 @@ function clica_calcula(){
     }else{
         erro_sal.style.display='none'
         result.style.display='inline'
+        /*SALÁRIO BRUTO*/
         let sal = parseFloat(sal_bruto.value)
         base_salary.innerHTML=`R$ ${sal.toFixed(2)}`   
         base_salary.style.color='rgb(19, 82, 119)'
         
+        /* DEPENDENTES  */
         let calc_dep = parseFloat(dep.value * 189.59)
         res_dep.innerHTML=`R$ ${calc_dep.toFixed(2)}`
 
+        /* OUTRAS DE DUÇÕES */
+        let deduct_val = parseFloat(deduct.value);
         if(deduct.value.length == 0){
+            deduct_val = 0;
             res_deduct.innerHTML='R$ 0,00'
         }else{
-            let deduct_val = parseFloat(deduct.value)
             res_deduct.innerHTML=`R$ ${deduct_val.toFixed(2)}`
         }
-        
 
+        /* CALCULO DO INSS */
+        let inss;
+        if (sal_bruto.value <= 1045){
+            inss = parseFloat(sal_bruto.value * 0.075)  
+        }else if (sal_bruto.value > 1045 && sal_bruto.value <= 2089.60){
+            inss = parseFloat((sal_bruto.value - 1045) * 0.09 + 78.38)           
+        }else if(sal_bruto.value > 2089.60 && sal_bruto.value <= 3134.40){
+            inss = parseFloat((sal_bruto.value - 2089.60) * 0.12 + 78.38 + 94.02)
+        }else if (sal_bruto.value > 3134.40 && sal_bruto.value <= 6101.06){
+            inss = parseFloat((sal_bruto.value - 3134.40) * 0.14 + 78.38 + 94.02 + 125.37)
+        }else{
+            inss = 713.10
+        }
+        res_inss.innerHTML=`R$ ${inss.toFixed(2)}`
+
+        /*CÁLCULO DO SALÁRIO LIQUIDO */
+        let sal_liquido = parseFloat(sal_bruto.value - inss -  deduct_val - calc_dep);
+
+        if(sal_liquido < 0){
+            res_liquido.innerHTML=`R$ ${sal_liquido.toFixed(2)}`
+            res_liquido.style.color='red'
+        }else{
+            res_liquido.innerHTML=`R$ ${sal_liquido.toFixed(2)}`
+            res_liquido.style.color='green'
+        }
+        
+        /*CÁLCULO DO IRRF */
+
+        let irrf;
+        if(sal_liquido <= 1903.98){
+            res_irrf.innerHTML=`Isento`
+        }else{
+            /*dif_irrf = parseFloat(sal_liquido - 1903.98)*/
+            if(sal_liquido >= 1903.00 && sal_liquido <= 2826.65 ){
+                irrf = parseFloat(sal_liquido * 0.075 - 142.80)
+            }else if(sal_liquido >= 2826.65 && sal_liquido <= 3751.05){
+                irrf = parseFloat((sal_liquido * 0.15) - 354.80)
+            }else if(sal_liquido >= 3751.05 && sal_liquido <= 4664.68){
+                irrf = parseFloat(sal_liquido * 0.2250 - 636.13)
+            }else{
+                irrf = parseFloat(sal_liquido * 0.2750 - 869.36)
+            }
+        }
+        res_irrf.innerHTML=`R$ ${irrf.toFixed(2)}`
     }
 }
 
@@ -85,4 +133,8 @@ function clica_limpar(){
     sal_bruto.value = null
     dep.value = null
     deduct.value = null
+}
+
+function clicar_calc_novamente(){
+    location.reload()
 }
